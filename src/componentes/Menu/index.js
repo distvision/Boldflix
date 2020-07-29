e2458648deb5bc12ba1f6d2dec983fac9fe68219
distvision <fredbfurede@gmail.com>
2020-07-29 16:35:20 +0200

    segundo comit

--- src/componentes/Menu/index.js
+++ src/componentes/Menu/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link } from 'react-router-dom'
 import Logo from '../../assets/img/Logo.png';
 import './Menu.css';
 import Button from '../Button'
@@ -8,11 +9,11 @@ import Button from '../Button'
 function Menu() {
     return (
         <nav className="Menu" >
-            <a  href="/">
-            <img className="Logo" src = {Logo} alt="Boldflixlogo" />
-            </a>
+            <Link  to="/">
+             <img className="Logo" src = {Logo} alt="Boldflixlogo" />
+            </Link>
 
-            <Button as="a" className="ButttonLink" href="/">
+            <Button as={Link} className="ButttonLink" to="/cadastro/video">
                 Novo vídeo
             </Button>
         </nav>
